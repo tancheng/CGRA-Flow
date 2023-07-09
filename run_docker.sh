de23ebacaa10e7a5b83aaf6f07a66fc97004f306
yyfHIT <yyf305016@163.com>
2023-07-08 22:43:16 -0700

    run_docker.sh updated

--- run_docker.sh
+++ run_docker.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-CONTAINER=cgra/cgra-flow:demo
+IMAGE=cgra/cgra-flow:demo
 COMMAND=/bin/bash
-NIC=en0
+NIC=$(sudo cat /proc/net/dev | awk 'END {print $1}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g')
 
 # Grab the ip address of this box
 IPADDR=$(ifconfig $NIC | grep "inet " | awk '{print $2}')
 
-DISP_NUM=$(jot -r 1 100 200)  # random display number between 100 and 200
+DISP_NUM=100 # fixed display number to 100 
 
-PORT_NUM=$((6000 + DISP_NUM)) # so multiple instances of the container won't interfer with eachother
+PORT_NUM=$((6000 + DISP_NUM))
 
-socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" 2>&1 > /dev/null &
+socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:/tmp/.X11-unix/X0 2>&1 > /dev/null &
 
 XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth.$USER.$$
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run \
+sudo docker run \
     -it \
-    --rm \
+    --name=CGRA-Flow \
     -v $XSOCK:$XSOCK:rw \
-    -v $XAUTH:$XAUTH:rw \
     -e DISPLAY=$IPADDR:$DISP_NUM \
-    -e XAUTHORITY=$XAUTH \
-    $CONTAINER
+    $IMAGE
+

9dca5a89f82d1737c5588e92d82e24be27d3644b
Wang-Yuanlu <y45760811@gmail.com>
2024-03-06 14:53:34 +0800

    Two errors have been figured out.

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IMAGE=cgra/cgra-flow:demo
-COMMAND=/bin/bash
+#COMMAND=/bin/bash
 NIC=$(sudo cat /proc/net/dev | awk 'END {print $1}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g')
 
 # Grab the ip address of this box
@@ -11,7 +11,7 @@ DISP_NUM=100 # fixed display number to 100
 
 PORT_NUM=$((6000 + DISP_NUM))
 
-socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:/tmp/.X11-unix/X0 2>&1 > /dev/null &
+socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:/tmp/.X11-unix/X0  > /dev/null 2>&1 &
 
 XSOCK=/tmp/.X11-unix
 

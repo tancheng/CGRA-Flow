bb8e18efa52c13bf24a2fe1af9163055914ae3af
yyan7223 <22B305016@stu.hit.edu.cn>
2024-09-23 05:59:23 -0700

    Have fixed DISPLAY conflict issues

--- start_docker.sh
+++ start_docker.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-CONTAINER=CGRA-Flow
-COMMAND=/bin/bash
+CONTAINER=CGRA-Flow-OpenRoad
 
-DISP_NUM=100 # fixed display number to 100 
-
-PORT_NUM=$((6000 + DISP_NUM))
-
-socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:/tmp/.X11-unix/X0 2>&1 > /dev/null &
-
-XSOCK=/tmp/.X11-unix
+xhost + 
 
 sudo docker start -i $CONTAINER

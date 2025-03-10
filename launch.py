6ac8de4cea14906c948f0cc3a16a07ae5008a91f
zhong qing <>
2025-03-09 19:01:17 -0700

    move debugging/theme into doc dir

--- launch.py
+++ launch.py
@@ -9,7 +9,7 @@ customtkinter.set_default_color_theme("dark-blue")
 
 master = customtkinter.CTk()
 script_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(script_dir, "theme")
+image_path = os.path.join(script_dir, "doc/theme")
 theme_selector_dark_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "dark_theme.png")), size=(500, 410))
 theme_selector_light_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "light_theme.png")), size=(500, 410))
 theme_selector_classic_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "classic_theme.png")), size=(500, 410))

dd4a10830dc01d764bddec03580423e2e59daa16
MeowMJ <2998673969@qq.com>
2026-01-19 12:24:27 +0000

    [UPDATE] run_windows_docker.sh and CGRA-Mapper branch

--- run_windows_docker.sh
+++ run_windows_docker.sh
@@ -7,10 +7,25 @@ CONTAINER=mycgraflow
 XSOCK=/tmp/.X11-unix
 
 sudo docker run \
-    -it \
-    --name=$CONTAINER \
-    -v $XSOCK:$XSOCK:rw \
-    -e DISPLAY=$DISPLAY \
-    $IMAGE \
-    /bin/bash
+	-it \
+	--name=$CONTAINER \
+	-v $XSOCK:$XSOCK:rw \
+	-e DISPLAY=$DISPLAY \
+	$IMAGE \
+	/bin/bash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
